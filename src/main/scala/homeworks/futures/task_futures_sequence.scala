497d5f0b060d98c0fb83d5d524c5b2324b1c2a1c
NEZAMETDINOV Eldar <Eldar.NEZAMETDINOV@raiffeisen.ru>
2020-12-20 03:27:38 +0300

    HW5. Scala Future

--- src/main/scala/homeworks/futures/task_futures_sequence.scala
+++ src/main/scala/homeworks/futures/task_futures_sequence.scala
@@ -1,11 +1,10 @@
 package homeworks.futures
 
-import homeworks.HomeworksUtils.TaskSyntax
-
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 object task_futures_sequence {
 
+  implicit val ec = ExecutionContext.global
   /**
    * В данном задании Вам предлагается реализовать функцию fullSequence,
    * похожую на Future.sequence, но в отличии от нее,
@@ -18,6 +17,19 @@ object task_futures_sequence {
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
-  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] =
-    task"Реализуйте метод `encrypt`"()
+  def fullSequence[A](futures: List[Future[A]]): Future[(List[A], List[Throwable])] = {
+    // замапили значения фьюч в Either
+    val ss: List[Future[Either[Throwable, A]]] = futures.map(_.map(Right(_)).recover{case e => Left(e)})
+    // сделали из списка фьюч - фьючу cо списком
+    val transformed: Future[List[Either[Throwable, A]]] = Future.sequence(ss)
+    // замапили выходной тип
+    transformed
+      .map { list: List[Either[Throwable, A]] =>
+        val (items, errors) = list.foldRight[(List[A], List[Throwable])](Nil, Nil) {
+          case (Left(error), (e, i)) => (e, error :: i)
+          case (Right(result), (e, i)) => (result :: e, i)
+        }
+        (items, errors)
+      }
+  }
 }
